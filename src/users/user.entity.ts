188b3c040d10f753bcb39e31dd421b46d1ac7e8c
fouad lmrini <fouad11.lamrini@gmail.com>
2026-01-02 00:22:35 +0100

    admin seeder

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -7,13 +7,13 @@ export class User extends Document {
   @Prop({ required: true })
   fullName: string;
 
-  @Prop({ required: true, unique: true })
+  @Prop({ required: true, unique: true, lowercase: true })
   email: string;
 
   @Prop({ required: true })
   password: string;
 
-  @Prop({ enum: Role, required: true })
+  @Prop({ type: String, enum: Role, required: true })
   role: Role;
 
   @Prop()
