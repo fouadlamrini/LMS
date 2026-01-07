c4b1299fbe52d2acbf6a3f0dc20e732993cc7844
ikrambenallali <ikrambenallali2003@gmail.com>
2026-01-07 20:35:35 +0100

    feat: add Trainer module with controller and service for course management

---
+++ src/trainer/trainer.service.ts
@@ -0,0 +1,53 @@
+import {
+  Injectable,
+  ForbiddenException,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Course, CourseDocument } from '../courses/schemas/course.schema';
+import { Enrollment, EnrollmentDocument } from 'src/enrollments/schemas/enrollment.schema';
+
+@Injectable()
+export class TrainerService {
+  constructor(
+    @InjectModel(Course.name)
+    private readonly courseModel: Model<CourseDocument>,
+    @InjectModel(Enrollment.name)
+    private readonly enrollmentModel: Model<EnrollmentDocument>,
+  ) {}
+
+  async verifyCourseOwnership(
+    courseId: string,
+    trainerId: string,
+  ) {
+    const course = await this.courseModel.findById(courseId);
+
+    if (!course) {
+      throw new NotFoundException('Course not found');
+    }
+
+    if (course.trainerId.toString() !== trainerId) {
+      throw new ForbiddenException(
+        'You are not allowed to access this course',
+      );
+    }
+
+    return course;
+  }
+  async getEnrolledLearners(
+  courseId: string,
+  trainerId: string,
+) {
+  // 🔐 US-7.3 : vérifier que le cours appartient au trainer
+  await this.verifyCourseOwnership(courseId, trainerId);
+
+  // récupérer les apprenants inscrits
+  const enrollments = await this.enrollmentModel
+    .find({ courseId })
+    .populate('userId', 'name email role');
+
+  return enrollments;
+}
+
+}
