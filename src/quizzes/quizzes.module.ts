d66d964c486be45b964363261f077dd0a9fe52a4
ElFirdaous28 <elmokhtarifirdaous@gmail.com>
2026-01-07 14:44:18 +0100

    LAN-19 LAN-109 LAN-111 quiz and quiz questions managment

--- src/quizzes/quizzes.module.ts
+++ src/quizzes/quizzes.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
-import { QuizzesService } from './quizzes.service';
-import { QuizzesController } from './quizzes.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { QuizzesService } from './services/quizzes.service';
+import { QuizzesController } from './controllers/quizzes.controller';
+import { Quiz, QuizSchema } from './schemas/quiz.schema';
+import { QuestionsController } from './controllers/questions.controller';
+import { QuestionsService } from './services/questions.service';
 
 @Module({
-  controllers: [QuizzesController],
-  providers: [QuizzesService],
+  imports: [
+    MongooseModule.forFeature([{ name: Quiz.name, schema: QuizSchema }]),
+  ],
+  controllers: [QuizzesController, QuestionsController],
+  providers: [QuizzesService, QuestionsService],
 })
 export class QuizzesModule {}
