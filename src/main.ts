583748d299f0daa8f115bb8fbd1130a1f29d919e
Ibrahim-Lmlilas <lmlilas.ibrahim@student.youcode.ma>
2026-01-09 06:18:41 +0100

    LAN-87

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { UsersService } from './users/users.service';
 import { seedAdmin } from './seeders/admin.seeder';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Serve static files (uploaded PDFs)
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
 
   // 🟢 get UsersService
   const usersService = app.get(UsersService);
