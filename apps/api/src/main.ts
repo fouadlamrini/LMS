6468a7946e9807763eceb8e0ac6d2239de2cac75
ElFirdaous28 <elmokhtarifirdaous@gmail.com>
2026-01-18 14:29:44 +0100

    globel prefix

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,16 +5,22 @@ import { seedAdmin } from './seeders/admin.seeder';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // Serve static files (uploaded PDFs)
-  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
-    prefix: '/uploads/',
+  // Enable cookie parsing
+  app.use(cookieParser());
+
+  // Enable CORS with credentials
+  app.enableCors({
+    origin: 'http://localhost:3000', // Next.js frontend URL
+    credentials: true, // Allow cookies
   });
 
   app.setGlobalPrefix('api');
+
 
   // ENABLE DTO VALIDATION
   app.useGlobalPipes(
@@ -25,13 +31,20 @@ async function bootstrap() {
     }),
   );
 
+  // Serve static files (uploaded PDFs)
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
+
+
   // get UsersService
   const usersService = app.get(UsersService);
-
   // seed admin once
   await seedAdmin(usersService);
 
-  await app.listen(process.env.PORT ?? 3000);
-  console.log('🚀 Server running on http://localhost:3000');
+  const PORT = process.env.PORT;
+  await app.listen(PORT || 3001);
+  console.log(`🚀 API running on http://localhost:${PORT}`);
+  
 }
 bootstrap();
