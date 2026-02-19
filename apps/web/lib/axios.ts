ea447afd1b0a19787793ae8916755ed549983e0f
ElFirdaous28 <elmokhtarifirdaous@gmail.com>
2026-02-19 21:34:55 +0000

    dep env test

--- apps/web/lib/axios.ts
+++ apps/web/lib/axios.ts
@@ -1,6 +1,8 @@
 // lib/axios.ts
 import axios from "axios";
 
+console.log(process.env.NEXT_PUBLIC_API_URL);
+
 const api = axios.create({
   baseURL: (process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001") + "/api",
   headers: {
