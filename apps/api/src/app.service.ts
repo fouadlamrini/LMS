bad5cb39f79c42e0b7224d14de44f7ec6dd174d3
ElFirdaous28 <elmokhtarifirdaous@gmail.com>
2026-02-19 22:31:40 +0000

    .

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `Hello World! :somthing from env: ${process.env.MONGODB_URI}`;
+    return 'Hello World!';
   }
 }
